faa7c944423f9e0498ae1021939e60c6256879a9
nebsdsamwu <nebsd.sam.wu@gmail.com>
2017-03-13 11:54:59 -0700

    Add project files.

---
+++ DinnerBot01/Dialogs/RootDialog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.FormFlow;
+using System.Threading.Tasks;
+
+namespace DinnerBot01.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            //throw new NotImplementedException();
+            await context.PostAsync("Welcome to Dinner Bot");
+            context.Wait(this.MessageReceiveAsync);
+        }
+
+        private async Task MessageReceiveAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            await context.PostAsync("How can I help you?");
+        }
+    }
+}
---
+++ DinnerBot01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DinnerBot01.WebApiApplication" Language="C#" %>
